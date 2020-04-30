2e3685bf2c6c01b5043f27ae8b6bfbe8b65cc943
Artem Diashkin <nemrosim1988@gmail.com>
2020-04-30 23:54:04 +0300

    add multibar with animation (full solution)

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ import 'widgets/raised_app_button.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/widgets/multibar.dart
+++ lib/widgets/multibar.dart
@@ -18,35 +18,38 @@ class _MultibarState extends State<Multibar>
     );
   }
 
+  void onPressedHandler() {
+    if (animationController.isDismissed) {
+      animationController.forward();
+    } else {
+      animationController.reverse();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Multibar"),
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.yellow,
       ),
       body: Container(
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: <Widget>[
-            StackWidget(animationController),
-          ],
-        ),
+        color: Colors.white,
+        width: double.infinity,
+        height: double.infinity,
+        child: IconsStackWidget(animationController),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blueGrey,
-        onPressed: () {
-          if (animationController.value == 0) {
-            animationController.forward(from: 0);
-          } else {
-            animationController.animateBack(0);
-          }
-        },
-        child: Icon(Icons.account_circle),
+        backgroundColor: Colors.yellow,
+        onPressed: onPressedHandler,
+        child: Icon(
+          Icons.settings,
+          size: 40,
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
-        color: Colors.blueGrey,
+        color: Colors.yellow,
         shape: const CircularNotchedRectangle(),
         child: Container(
           height: 40.0,
@@ -56,130 +59,60 @@ class _MultibarState extends State<Multibar>
   }
 }
 
-class StackWidget extends StatefulWidget {
-  AnimationController animationController;
+class IconsStackWidget extends StatelessWidget {
+  final AnimationController animationController;
 
-  StackWidget(this.animationController);
+  static const double RIGHT_END_POINT = 100;
+  static const double SECOND_RIGHT_END_POINT = 60;
+  static const double TOP_END_POINT = 40;
+  static const double SECOND_TOP_END_POINT = 90;
 
-  @override
-  _StackWidgetState createState() => _StackWidgetState();
-}
+  IconsStackWidget(this.animationController);
 
-class _StackWidgetState extends State<StackWidget> {
-  double _iconSize = 65.0;
-  Animation delayedAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  double moveTo(double value) {
-    return value * widget.animationController.value;
+  Animation getDelayedAnimation(double delay) {
+    return Tween(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(
+        parent: this.animationController,
+        curve: Interval(
+          delay,
+          1.0,
+          curve: Curves.easeOut,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    double rightCenter = MediaQuery.of(context).size.width / 2 - _iconSize / 2;
-    double bottomCenter = -_iconSize / 2;
-
-    delayedAnimation = Tween(begin: rightCenter, end: bottomCenter).animate(
-      CurvedAnimation(
-        parent: widget.animationController,
-        curve: Interval(
-          0.5,
-          1.0,
-          curve: Curves.fastOutSlowIn,
-        ),
-      ),
-    );
-
-    double start = 610;
-    double right = 140;
     return AnimatedBuilder(
-        animation: widget.animationController,
+        animation: this.animationController,
         builder: (context, _) {
           return Stack(
             children: <Widget>[
               MovableIcon(
-                right: right,
-                left: start,
-                rightEnd: 100,
-                leftEnd: 40,
-                animation: Tween(begin: 1.0, end: 0.0).animate(
-                  CurvedAnimation(
-                    parent: widget.animationController,
-                    curve: Interval(
-                      0.60,
-                      1.0,
-                      curve: Curves.fastOutSlowIn,
-                    ),
-                  ),
-                ),
+                rightEnd: RIGHT_END_POINT,
+                topEnd: TOP_END_POINT,
+                animation: getDelayedAnimation(0.60),
               ),
               MovableIcon(
-                right: right,
-                left: start,
-                rightEnd: 60,
-                leftEnd: 90,
-                animation: Tween(begin: 1.0, end: 0.0).animate(
-                  CurvedAnimation(
-                    parent: widget.animationController,
-                    curve: Interval(
-                      0.45,
-                      1.0,
-                      curve: Curves.fastOutSlowIn,
-                    ),
-                  ),
-                ),
+                rightEnd: SECOND_RIGHT_END_POINT,
+                topEnd: SECOND_TOP_END_POINT,
+                animation: getDelayedAnimation(0.45),
               ),
               MovableIcon(
-                right: right,
-                left: start,
                 rightEnd: 0,
-                leftEnd: 110,
-                animation: Tween(begin: 1.0, end: 0.0).animate(
-                  CurvedAnimation(
-                    parent: widget.animationController,
-                    curve: Interval(
-                      0.30,
-                      1.0,
-                      curve: Curves.fastOutSlowIn,
-                    ),
-                  ),
-                ),
+                topEnd: 115,
+                animation: getDelayedAnimation(0.30),
               ),
               MovableIcon(
-                right: right,
-                left: start,
-                rightEnd: -60,
-                leftEnd: 90,
-                animation: Tween(begin: 1.0, end: 0.0).animate(
-                  CurvedAnimation(
-                    parent: widget.animationController,
-                    curve: Interval(
-                      0.15,
-                      1.0,
-                      curve: Curves.fastOutSlowIn,
-                    ),
-                  ),
-                ),
+                rightEnd: -SECOND_RIGHT_END_POINT,
+                topEnd: SECOND_TOP_END_POINT,
+                animation: getDelayedAnimation(0.15),
               ),
               MovableIcon(
-                right: right,
-                left: start,
-                rightEnd: -100,
-                leftEnd: 40,
-                animation: Tween(begin: 1.0, end: 0.0).animate(
-                  CurvedAnimation(
-                    parent: widget.animationController,
-                    curve: Interval(
-                      0.0,
-                      1.0,
-                      curve: Curves.fastOutSlowIn,
-                    ),
-                  ),
-                ),
+                rightEnd: -RIGHT_END_POINT,
+                topEnd: TOP_END_POINT,
+                animation: getDelayedAnimation(0.00),
               )
             ],
           );
@@ -188,32 +121,35 @@ class _StackWidgetState extends State<StackWidget> {
 }
 
 class MovableIcon extends StatelessWidget {
-  double right;
-  double left;
+  final double rightEnd;
+  final double topEnd;
 
-  double rightEnd;
-  double leftEnd;
+  final Animation animation;
 
-  Animation animation;
-
-  MovableIcon(
-      {this.right, this.left, this.rightEnd, this.leftEnd, this.animation});
+  MovableIcon({this.rightEnd, this.topEnd, this.animation});
 
   @override
   Widget build(BuildContext context) {
-    double iconSize = 45.0;
+    double topStart = 610;
+    double rightStart = 143;
 
     return Transform(
-      transform: Matrix4.translationValues(right - rightEnd * animation.value,
-          left - leftEnd * animation.value, 0),
-      child: RaisedButton(
-        onPressed: () {},
-        shape: CircleBorder(),
-        elevation: 5 * animation.value,
-        child: Icon(
-          Icons.security,
-          color: Colors.blueGrey,
-          size: iconSize,
+      transform: Matrix4.translationValues(
+          rightStart - rightEnd * animation.value,
+          topStart - topEnd * animation.value,
+          0),
+      child: SizedBox(
+        height: 54,
+        child: RaisedButton(
+          onPressed: () {},
+          shape: CircleBorder(),
+          elevation: 5 * animation.value,
+          color: Colors.yellow,
+          child: Icon(
+            Icons.security,
+            color: Colors.black,
+            size: 30,
+          ),
         ),
       ),
     );
